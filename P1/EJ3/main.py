c9b923b98ea583925c78aa41531ae8287d7fe3f7
blancagiron <blancagiron@correo.ugr.es>
2025-03-11 00:51:15 +0100

    el usuario puede elegir la estrategia de scraping

--- P1/EJ3/main.py
+++ P1/EJ3/main.py
@@ -11,16 +11,21 @@ def pasar_a_yaml(datos, filename):
 def main():
     
     url = "https://quotes.toscrape.com"
-    context = Context(BeautifulSoupScrap())
-    datosBeautifulSoup = context.scrape(url)
-    # Guardamos los datos en un archivo yaml
-    pasar_a_yaml(datosBeautifulSoup, "quotesBeautifulSoup.yaml")
+   
+    # Solicitamos al usuario que elija la estrategia de scraping
+    # Se realiza el scraping y se guarda en un archivo yaml
+    estrategia = input("Elija la estrategia de scraping (1 para BeautifulSoup, 2 para Selenium): ")
     
-    
-    # Cambiamos la estrategia a Selenium
-    context.set_strategy(SeleniumStrategy())
-    datosSelenium = context.scrape(url)
-    pasar_a_yaml(datosSelenium, "quotesSelenium.yaml")
+    if estrategia == "1":
+        context = Context(BeautifulSoupScrap())
+        datos = context.scrape(url)
+        pasar_a_yaml(datos, "quotesBeautifulSoup.yaml")
+    elif estrategia == "2":
+        context = Context(SeleniumStrategy())
+        datos = context.scrape(url)
+        pasar_a_yaml(datos, "quotesSelenium.yaml")
+    else:
+        print("Estrategia no válida. Por favor, elija 1 o 2.")
     
 if __name__ == "__main__":
     main()
