9a7b34341015ee56e600d98bd626be879fbf0b7e
Pablo Tamayo Lopez <130286743+pablotl0@users.noreply.github.com>
2025-03-23 19:26:40 +0100

    Delete ej4 directory

--- ej4/filters/base.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Filter(ABC):
-    @abstractmethod
-    def execute(self, request):
-        pass
--- ej4/services/authentication_service.py
+++
@@ -1,6 +0,0 @@
-class AuthenticationService:
-    def process(self, request):
-        return {
-            'success': True,
-            'message': f"Autenticación exitosa para el usuario {request.get('email')}"
-        }
