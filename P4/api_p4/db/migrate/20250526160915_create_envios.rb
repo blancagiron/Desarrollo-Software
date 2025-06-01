1a0a778238e4ebcac346a0835f81581fffd61459
Pablo Tamayo Lopez <130286743+pablotl0@users.noreply.github.com>
2025-06-01 21:13:18 +0200

    Update 20250526160915_create_envios.rb

--- P4/api_p4/db/migrate/20250526160915_create_envios.rb
+++ P4/api_p4/db/migrate/20250526160915_create_envios.rb
@@ -1,6 +1,7 @@
 class CreateEnvios < ActiveRecord::Migration[7.0]
   def change
-    create_table :envios do |t|
+    create_table :envios, id: false do |t| # ⬅️ Disable autoincrement ID
+      t.primary_key :id, :integer # ⬅️ ID personalizado, tipo entero
       t.json :sillas
       t.integer :estado, default: 0
       t.string :direccion
